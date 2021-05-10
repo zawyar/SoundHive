8ceb7814d6bd380cd267011a7003936e14c36124
Zawyar Ur Rehman <zawyar8000@gmail.com>
2021-05-10 21:27:24 +0500

    Added admin pages

---
+++ SoundHive/Admin.Master.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SoundHive
+{
+    public partial class SiteMaster : MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["username"] == null)
+            {
+                // Response.Redirect("Login.aspx");
+                username_placeholder.InnerText = "Generic User";
+            }
+            else
+            {
+                username_placeholder.InnerText = Convert.ToString(Session["username"]);
+            }
+
+        }
+    }
+}
